dcc45701b44b9b7e11ee1669c23d727171607fa0
Andy Wendt <andy@awendt.com>
2019-06-07 08:01:52 -0600

    Add button for click event

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {getFunName} from "../helpers";
 
 class StorePicker extends React.Component {
+    handleClick() {
+        alert("heyyyy!!!");
+    }
     render() {
         return (
           <form className="store-selector">
               <h2>Please Enter A Store</h2>
+              <button onClick={this.handleClick}>Click me!</button>
               <input
                   type="text"
                   required
