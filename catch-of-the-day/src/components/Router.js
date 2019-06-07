12c8a561ea54c63de81d0d23dce2d76d59097ce8
Andy Wendt <andy@awendt.com>
2019-06-07 07:50:39 -0600

    Remove exact

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -8,7 +8,7 @@ const Router = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={StorePicker}/>
-            <Route exact path="/store/:storeId" component={App}/>
+            <Route path="/store/:storeId" component={App}/>
             <Route component={NotFound}/>
         </Switch>
     </BrowserRouter>
