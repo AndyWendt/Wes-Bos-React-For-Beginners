ad139d49db4ddf3878d276ad24db35676d0e553b
Andy Wendt <andy@awendt.com>
2019-06-06 18:31:58 -0600

    Fix tagline

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header tagline="Yo! Cool"/>
+                    <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
                 <Inventory />
