8d1ff4bddca175ede44c7abb8a67108633901b0c
Andy Wendt <andy@awendt.com>
2019-06-10 08:24:56 -0600

    Set destination for form

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
-    render() {
-        return (
-             <form className="fish-edit">
-               <input name="name" type="text" placeholder="name" />
-               <input name="price" type="text" placeholder="price" />
-               <select name="status">
-                  <option value="available">Fresh!</option>
-                  <option value="unavailable">Sold Out!</option>
-               </select>
-               <textarea name="desc" placeholder="desc" />
-               <input name="image" type="text" placeholder="image" />
-               <button type="submit">+ Add Fish</button>
-             </form>
-        );
-    }
+
+  createFish = (event) => {
+    event.preventDefault();
+    console.log("making the fish")
+
+  };
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" type="text" placeholder="name"/>
+        <input name="price" type="text" placeholder="price"/>
+        <select name="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" placeholder="desc"/>
+        <input name="image" type="text" placeholder="image"/>
+        <button type="submit">+ Add Fish</button>
+      </form>
+    );
+  }
 }
 
 export default AddFishForm;
