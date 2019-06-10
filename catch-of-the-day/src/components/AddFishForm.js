9f565799046a4775bdf2b2c9017bd072db36419e
Andy Wendt <andy@awendt.com>
2019-06-10 08:20:51 -0600

    Finish up form

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -3,8 +3,17 @@ import React from 'react';
 class AddFishForm extends React.Component {
     render() {
         return (
-             <form>
+             <form className="fish-edit">
                  <p>It works</p>
+               <input name="name" type="text" placeholder="name" />
+               <input name="price" type="text" placeholder="price" />
+               <select name="status">
+                  <option value="available">Fresh!</option>
+                  <option value="unavailable">Sold Out!</option>
+               </select>
+               <textarea name="desc" placeholder="desc" />
+               <input name="image" type="text" placeholder="image" />
+               <button type="submit">Submit</button>
              </form>
         );
     }
