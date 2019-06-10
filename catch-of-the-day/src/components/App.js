2fa2db79e8b01d29f204800f93e1f3fbe088567f
Andy Wendt <andy@awendt.com>
2019-06-10 13:52:51 -0600

    Add state to app

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,17 +4,22 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market"/>
-                </div>
-                <Order />
-                <Inventory />
-            </div>
-        );
-    }
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    );
+  }
 }
 
 export default App;
