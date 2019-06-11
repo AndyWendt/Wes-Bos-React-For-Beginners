c0ac83ae549375963091119a8a07363abbe9b052
Andy Wendt <andy@awendt.com>
2019-06-10 18:34:25 -0600

    Add fishname

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -5,6 +5,7 @@ class Fish extends React.Component {
         return (
             <li className="menu-fish">
                 <img src={this.props.details.image} alt={this.props.details.name} />
+                <h3 className="fish-name">{this.props.details.name}</h3>
             </li>
         );
     }
