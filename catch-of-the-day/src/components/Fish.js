1c3ef8908fae01488375ced56d4d15ad4cb80288
Andy Wendt <andy@awendt.com>
2019-06-10 18:26:03 -0600

    Keys for sample fish displaying

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Fish extends React.Component {
+    render() {
+        return (
+            <div className="single-fish">Fish!!!</div>
+        );
+    }
+}
+
+export default Fish;
