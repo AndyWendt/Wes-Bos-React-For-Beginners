630e3a008d0005c96b3fc4eae0b5b3f70137b3e4
Andy Wendt <andy@awendt.com>
2019-06-12 08:22:09 -0600

    Update order to add key to an iterated item.

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -7,10 +7,10 @@ class Order extends React.Component {
       const count = this.props.order[key];
       const isAvailable = fish.status === 'available';
       if (!isAvailable) {
-        return <li>Sorry {fish ? fish.name : 'fish'} is no longer available</li>;
+        return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>;
       }
 
-      return <li>
+      return <li key={key}>
         {count} lbs {fish.name}
         {formatPrice(count * fish.price)}
       </li>;
