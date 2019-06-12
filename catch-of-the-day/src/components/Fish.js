f87ba34fcc026a07d16e98b89387c028173bff40
Andy Wendt <andy@awendt.com>
2019-06-11 18:28:44 -0600

    Update fish component

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 class Fish extends React.Component {
-    render() {
-        return (
-            <li className="menu-fish">
-                <img src={this.props.details.image} alt={this.props.details.name} />
-                <h3 className="fish-name">{this.props.details.name}</h3>
-            </li>
-        );
-    }
+  render() {
+    const { image, name, price, description, status } = this.props.details;
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name}/>
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{price}</span>
+        </h3>
+      </li>
+    );
+  }
 }
 
 export default Fish;
