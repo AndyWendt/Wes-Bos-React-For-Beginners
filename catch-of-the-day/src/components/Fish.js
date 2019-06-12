7eea89b72dd602d9ec7a58451d993d1607d84b54
Andy Wendt <andy@awendt.com>
2019-06-12 07:33:48 -0600

    Add disabled prop to add to cart button

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -4,6 +4,8 @@ import { formatPrice } from '../helpers';
 class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === 'available';
+
     return (
       <li className="menu-fish">
         <img src={image} alt={name}/>
@@ -12,7 +14,7 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to Cart</button>
+        <button disabled={!isAvailable} >Add to Cart</button>
       </li>
     );
   }
