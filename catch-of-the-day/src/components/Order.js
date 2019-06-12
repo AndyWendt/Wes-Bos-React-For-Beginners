99522e885bf5a2b5618b8c3547e69a3ccb06e71c
Andy Wendt <andy@awendt.com>
2019-06-12 08:07:52 -0600

    Total working for order

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
     render() {
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((previousTotal, key) => {
+          const fish = this.props.fishes[key];
+          const count = this.props.order[key];
+          const isAvailable = fish && fish.status === 'available';
+
+          if (isAvailable) {
+            return previousTotal + (count * fish.price)
+          }
+
+          return previousTotal;
+        }, 0);
+
         return (
-            <div className="order">Order!!!</div>
+            <div className="order-wrap">
+                <h2>Order</h2>
+                <div className="total">Total: <strong>{formatPrice(total)}</strong></div>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
         );
     }
 }
