0aec563ca92fe70ca381f3d9aba748f92068ea3e
Andy Wendt <andy@awendt.com>
2019-06-13 07:46:39 -0600

    Add base.js and auth example

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+import auth from 'auth.json';
+
+const firebaseApp = firebase.initializeApp(auth);
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp }
