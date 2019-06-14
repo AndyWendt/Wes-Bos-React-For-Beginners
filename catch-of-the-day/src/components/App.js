666915cd43f881da7feb43cd5e4ee8c67c3b05b7
Andy Wendt <andy@awendt.com>
2019-06-14 14:31:08 -0600

    Add remove from order functionality

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -70,6 +70,12 @@ class App extends React.Component {
     this.setState({ order });
   };
 
+  removeFromOrder = key => {
+    const order = { ...this.state.order };
+    delete order[key];
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -79,7 +85,11 @@ class App extends React.Component {
             {Object.keys(this.state.fishes).map((key) => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -16,6 +16,7 @@ class Order extends React.Component {
       return <li key={key}>
         {count} lbs {fish.name}
         {formatPrice(count * fish.price)}
+        <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
       </li>;
     };
 
