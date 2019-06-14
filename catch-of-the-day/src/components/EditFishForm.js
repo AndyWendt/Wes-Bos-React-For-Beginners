8cc5151cabf6169a6c30039b66bb52f32ba88a47
Andy Wendt <andy@awendt.com>
2019-06-14 08:30:34 -0600

    Edit fish form pulling in new data

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class EditFishForm extends React.Component {
   render() {
-    return <p>Edit Fish!!!</p>;
+    return <div className="fish-edit">
+      <input type="text" name="name" value={this.props.fish.name} />
+      <input type="text" name="price" value={this.props.fish.price} />
+      <select type="text" name="status" value={this.props.fish.status} >
+        <option value="available">Fresh!</option>
+        <option value="unavailable">Sold Out!</option>
+      </select>
+      <textarea name="desc" value={this.props.fish.desc} />
+      <input type="text" name="image" value={this.props.fish.image} />
+    </div>
   }
 }
 
